ee2999ddfd48dec02b5acaca11c8159825f3a4d6
Olonde Felix <olondenarkiso@gmail.com>
2018-11-09 17:32:01 +0300

    file restructuring

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
       <h1>Welcome to Crypto Dashboard</h1>
       </div>
     );
