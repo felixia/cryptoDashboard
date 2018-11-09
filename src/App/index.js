6a6fb2a3decdc451376ce92b1431262c235c186b
Olonde Felix <olondenarkiso@gmail.com>
2018-11-09 19:06:26 +0300

    AppBar styling

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import './App.css';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
+      <AppBar/>
       <WelcomeMessage/>
 
       </AppLayout>
