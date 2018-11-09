bc66aa61b7e80afe86098e2b902657276b13faac
Olonde Felix <olondenarkiso@gmail.com>
2018-11-09 18:05:06 +0300

    Add AppLayout

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import WelcomeMessage from './WelcomeMessage';
 import './App.css';
+import AppLayout from './AppLayout';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <h1>Welcome to Crypto Dashboard</h1>
-      </div>
+      <AppLayout>
+      <WelcomeMessage/>
+
+      </AppLayout>
     );
   }
 }
